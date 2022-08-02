9ad14980eb9f82c0a79212df600d84b0d47ec1c9
DreSean-Sutton <dreseansutton@gmail.com>
2022-08-02 01:04:25 -0700

    NOW added a couple ghosts to the database, faileled to save

--- database/data.sql
+++ database/data.sql
@@ -20,3 +20,16 @@ VALUES
   ('Johnson'),
   ('Jones'),
   ('Wilson');
+
+INSERT INTO public.ghosts ("name", "description")
+VALUES
+  ('Caoineag', 'Whoever a Caoineag has marked will be the next to die. With their whispers, these entities foreshadow inevitable death.'),
+  ('Djinn', 'Ancient Spirits that rarely abandon their habitat and are incredibly hostile towards whoever invades their area. They love the light and often play with electrical appliances.'),
+  ('Doppler', 'They say if you stare in to the mirror, your disfigured twin will stare back. The twin, also called the Dopplganger, drains your victim''s vital power, acquiring his strength.'),
+  ('Onryo', 'The Onryo is an exceptionally aggressive ghost who will willingly make their presence known. They are capable of affecting your sanity with whispers, making a considerable impact on your sanity level'),
+  ('Mononoke', 'The Mononoke is driven by wrath. With each of their actions marked by pronounced hatred'),
+  ('Myling', 'The Myling is the most communicative ghost and will try to draw your attention by all means'),
+  ('Revenant', 'The Revenant is one of the most dangerous ghosts. They are relatively calm if you don''t agitate them and is often difficult to identify. Once they start hunting, very little will stop him.'),
+  ('Shiryo', 'The Shiryo will do the best to show you that you don''t have long to live. They are known to be extremely aggressive.'),
+  ('Shinigami', 'The Shinigami are drawn towards death, and they''re always near places of death.'),
+  ('Poltergeists are exceptionally active ghosts and like playing with the objects around their victims to frighten them.),
